1af5e622f45cb7bfa3163ef5d1b755d22ec04f0c
RalucaMorari <141261909+RalucaMorari@users.noreply.github.com>
2023-08-08 21:51:40 +0300

    Delete src/ARRAYS directory

--- src/ARRAYS/EX2ARRAYS.java
+++
@@ -1,8 +0,0 @@
-package ARRAYS;
-
-public class EX2ARRAYS {
-    public static void main(String[] args) {
-        //2. Scrie un program care sa adune toate preturile dintr-o lista (ca mai apoi sa fie afisata suma totala in cosul de cumparaturi):
-        //De exemplu, pentru {1, 7, 3, 10, 9}, se va afisa in consola valoarea 30.
-    }
-}
--- src/ARRAYS/EX3ARRAYS.java
+++
@@ -1,8 +0,0 @@
-package ARRAYS;
-
-public class EX3ARRAYS {
-    public static void main(String[] args) {
-        //3. Scrie un program care sa afiseze de cate ori apare un anumit numar n (citit de la tastatura) intr-un array.
-        //De exemplu, pentru {1, 2, 2, 3, 3, 3, 4, 4, 4, 4} si n=3, se va afisa "3 apare de 3 ori".
-    }
-}
