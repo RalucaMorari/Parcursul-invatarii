fc9190d2213834216b7ff34fbe0d0f27d2d1eb52
RalucaMorari <141261909+RalucaMorari@users.noreply.github.com>
2023-08-08 21:50:09 +0300

    Delete EX1ARRAYS.java

--- src/ARRAYS/EX1ARRAYS.java
+++
@@ -1,8 +0,0 @@
-package ARRAYS;
-
-public class EX1ARRAYS {
-    public static void main(String[] args) {
-        //1. Scrie un program care sa caute un numar de telefon intr-o lista de numere. Programul va printa “numarul a fost gasit” sau “numarul nu a fost gasit”
-    }
-
-}
